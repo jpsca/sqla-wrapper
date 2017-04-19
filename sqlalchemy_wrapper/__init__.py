be117dad00c5a36019dfc341805cb8ca06997b98
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2017-04-19 15:36:34 -0500

    v1.8.0

--- sqlalchemy_wrapper/__init__.py
+++ sqlalchemy_wrapper/__init__.py
@@ -6,17 +6,15 @@
 
     A friendly wrapper for SQLAlchemy.
 
-    :copyright: 2013-2015 by `Juan-Pablo Scaletti <http://jpscaletti.com>`_.
+    :copyright: 2013-2017 by `Juan-Pablo Scaletti <http://jpscaletti.com>`_.
     :license: BSD, see LICENSE for more details.
 
     Some of the code was extracted and adapted from
     Flask-SQLAlchemy <http://flask-sqlalchemy.pocoo.org/>
-    :copyright: 2010-2014 by Armin Ronacher.
-    :license: BSD, see LICENSE for more details.
 
 """
 from .main import SQLAlchemy, BaseQuery, Model  # noqa
 from .paginator import Paginator, sanitize_page_number  # noqa
 from .helpers import get_debug_queries  # noqa
 
-__version__ = '1.7.0'
+__version__ = '1.8.0'
